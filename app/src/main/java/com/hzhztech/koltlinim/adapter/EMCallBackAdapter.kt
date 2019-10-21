eaa979082cd9616fd90bd3d8e16a06570c676c1b
Lc <641229781@qq.com>
2019-10-21 13:20:31 +0800

    Login加入登录成功的回调

---
+++ app/src/main/java/com/hzhztech/koltlinim/adapter/EMCallBackAdapter.kt
@@ -0,0 +1,18 @@
+package com.hzhztech.koltlinim.adapter
+
+import com.hyphenate.EMCallBack
+
+open class EMCallBackAdapter :EMCallBack {
+    override fun onSuccess() {
+
+    }
+
+    override fun onProgress(p0: Int, p1: String?) {
+
+    }
+
+    override fun onError(p0: Int, p1: String?) {
+
+    }
+
+}
--- app/src/main/java/com/hzhztech/koltlinim/presenter/LoginPresenter.kt
+++ app/src/main/java/com/hzhztech/koltlinim/presenter/LoginPresenter.kt
@@ -1,8 +1,13 @@
 package com.hzhztech.koltlinim.presenter
 
+import com.blankj.utilcode.util.LogUtils
+import com.hyphenate.EMCallBack
+import com.hyphenate.chat.EMClient
+import com.hzhztech.koltlinim.adapter.EMCallBackAdapter
 import com.hzhztech.koltlinim.contract.LoginContract
 import com.hzhztech.koltlinim.extentions.isValidPassword
 import com.hzhztech.koltlinim.extentions.isValidUserName
+import kotlin.math.log
 
 class LoginPresenter(val view: LoginContract.View):LoginContract.LoginPresenter {
 
@@ -19,6 +24,20 @@ class LoginPresenter(val view: LoginContract.View):LoginContract.LoginPresenter 
     }
 
     private fun loginEaseMob(userName: String, password: String) {
+        EMClient.getInstance().login(userName,password,object : EMCallBackAdapter() {
+            //在子线程回调
+            override fun onSuccess() {
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+                LogUtils.e("登录聊天服务器成功")
+                //在主线程通知View层
+                uiThread { view.onLoggedInSuccess() }
+            }
 
+            override fun onError(p0: Int, p1: String?) {
+                super.onError(p0, p1)
+                uiThread { view.onLOggedInFailed() }
+            }
+        })
     }
 }
