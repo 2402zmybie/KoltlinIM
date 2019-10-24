061dff831fb861254bf38944561f6122ee66e3e1
Lc <641229781@qq.com>
2019-10-24 14:10:34 +0800

    长按弹出删除好友的对话框--hr

--- app/src/main/java/com/hzhztech/koltlinim/adapter/ContractListAdapter.kt
+++ app/src/main/java/com/hzhztech/koltlinim/adapter/ContractListAdapter.kt
@@ -1,9 +1,12 @@
 package com.hzhztech.koltlinim.adapter
 
 import android.content.Context
+import android.content.DialogInterface
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.hzhztech.koltlinim.R
 import com.hzhztech.koltlinim.data.ContactListItem
 import com.hzhztech.koltlinim.ui.activity.ChatActivity
 import com.hzhztech.koltlinim.widget.ContractListItemView
@@ -26,6 +29,22 @@ class ContractListAdapter(val context: Context, val contactListItems: MutableLis
         //点击联系人跳转到聊天界面
         val userName = contactListItems.get(position).userName
         contractListItemView.setOnClickListener { context.startActivity<ChatActivity>("username" to userName) }
+        contractListItemView.setOnLongClickListener {
+            val message = String.format(context.getString(R.string.delete_friend_message), userName)
+            AlertDialog.Builder(context).setTitle(R.string.delete_friend_title)
+                .setMessage(message)
+                .setNegativeButton(R.string.cancel,null)
+                .setPositiveButton(R.string.confirm, DialogInterface.OnClickListener { dialogInterface, i ->
+                    deleteFriend(userName)
+                })
+                .show()
+            true
+
+        }
+    }
+
+    private fun deleteFriend(userName: String) {
+
     }
 
     class ContractListItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
