4202cfe9acf1d68dffb3466a49f6cde779da413b
Lc <641229781@qq.com>
2019-10-25 17:42:26 +0800

    添加好友请求

--- app/src/main/java/com/hzhztech/koltlinim/widget/AddFriendListItemView.kt
+++ app/src/main/java/com/hzhztech/koltlinim/widget/AddFriendListItemView.kt
@@ -4,11 +4,15 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
+import com.hyphenate.chat.EMClient
 import com.hzhztech.koltlinim.R
+import com.hzhztech.koltlinim.adapter.EMCallBackAdapter
 import com.hzhztech.koltlinim.data.AddFriendItem
 import kotlinx.android.synthetic.main.view_add_friend_item.view.*
 import kotlinx.android.synthetic.main.view_contact_item.view.*
 import kotlinx.android.synthetic.main.view_contact_item.view.userName
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.toast
 
 class AddFriendListItemView(context: Context?, attrs: AttributeSet? = null) : RelativeLayout(context, attrs) {
 
@@ -26,8 +30,24 @@ class AddFriendListItemView(context: Context?, attrs: AttributeSet? = null) : Re
             add.isEnabled = true
             add.text = context.getString(R.string.add)
         }
+        add.setOnClickListener {
+            addFriend(addFriendItem.userName)
+        }
         userName.text = addFriendItem.userName
         timestamp.text = addFriendItem.timestamp
+    }
+
+    private fun addFriend(userName: String) {
+        EMClient.getInstance().contactManager().aysncAddContact(userName,null, object : EMCallBackAdapter() {
+            override fun onSuccess() {
+                context.runOnUiThread { toast(R.string.send_add_friend_success) }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                context.runOnUiThread { toast(R.string.send_add_friend_failed) }
+            }
+        })
+
     }
 
 }
