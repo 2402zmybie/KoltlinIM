b0d4fb332e8127b3fd76e049ef3e073dbc04e13f
Lc <641229781@qq.com>
2019-10-25 16:33:06 +0800

    查询所有的联系人

--- app/src/main/java/com/hzhztech/koltlinim/data/db/IMDatabase.kt
+++ app/src/main/java/com/hzhztech/koltlinim/data/db/IMDatabase.kt
@@ -1,7 +1,9 @@
 package com.hzhztech.koltlinim.data.db
 
 import com.hzhztech.koltlinim.extentions.toVarargArray
+import org.jetbrains.anko.db.MapRowParser
 import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
 
 class IMDatabase  {
 
@@ -14,6 +16,16 @@ class IMDatabase  {
         databaseHelper.use {
             //SQLiteDatabase的扩展方法
             insert(ContactTable.NAME, *contact.map.toVarargArray())
+        }
+    }
+
+    fun getAllContacts(): List<Contact> {
+        return databaseHelper.use {
+            select(ContactTable.NAME).parseList(object : MapRowParser<Contact> {
+                override fun parseRow(columns: Map<String, Any?>): Contact {
+                    return Contact(columns.toMutableMap())
+                }
+            })
         }
     }
 }
