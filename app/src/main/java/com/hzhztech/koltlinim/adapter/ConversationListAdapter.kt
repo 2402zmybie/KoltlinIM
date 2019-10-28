5e787d1e189c7dc05ef2a4dab0f063b1d54cfb5b
Lc <641229781@qq.com>
2019-10-28 16:14:33 +0800

    会话数据的加载--hr

--- app/src/main/java/com/hzhztech/koltlinim/adapter/ConversationListAdapter.kt
+++ app/src/main/java/com/hzhztech/koltlinim/adapter/ConversationListAdapter.kt
@@ -4,15 +4,19 @@ import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.hyphenate.chat.EMConversation
 import com.hzhztech.koltlinim.widget.ConversationListItemView
 
-class ConversationListAdapter(val context:Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ConversationListAdapter(
+    val context: Context,
+    val conversations: MutableList<EMConversation>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
         return ConversationListItemViewHolder(ConversationListItemView(context))
     }
 
-    override fun getItemCount(): Int = 30
+    override fun getItemCount(): Int = conversations.size
 
     override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
     }
--- app/src/main/java/com/hzhztech/koltlinim/ui/fragment/ConversationFragment.kt
+++ app/src/main/java/com/hzhztech/koltlinim/ui/fragment/ConversationFragment.kt
@@ -1,12 +1,19 @@
 package com.hzhztech.koltlinim.ui.fragment
 
 import android.support.v7.widget.LinearLayoutManager
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMConversation
 import com.hzhztech.koltlinim.R
 import com.hzhztech.koltlinim.adapter.ConversationListAdapter
-import kotlinx.android.synthetic.main.fragment_contacts.*
+import kotlinx.android.synthetic.main.fragment_contacts.recyclerView
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class ConversationFragment :BaseFragment() {
+
+    val conversations = mutableListOf<EMConversation>()
+
     override fun getLayoutResId(): Int  = R.layout.fragment_conversation
 
     override fun init() {
@@ -16,7 +23,19 @@ class ConversationFragment :BaseFragment() {
         recyclerView.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
-            adapter = ConversationListAdapter(context)
+            adapter = ConversationListAdapter(context,conversations)
+        }
+
+        loadConversations()
+    }
+
+    private fun loadConversations() {
+        doAsync {
+            val allConversations = EMClient.getInstance().chatManager().allConversations
+            conversations.addAll(allConversations.values)
+            uiThread {
+                recyclerView.adapter!!.notifyDataSetChanged()
+            }
         }
     }
 
